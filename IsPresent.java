431ee4ce3bd2e067148d91f6a299e875ebbc0d37
komalarao <manoharvakamullu1999@gmail.com>
2022-11-14 18:22:30 -0800

    day3- Uc1 problem

---
+++ IsPresent.java
@@ -0,0 +1,15 @@
+package bridgelabz;
+
+public class IsPresent {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to employee wage computation program on master branch");
+		double chek= Math.floor(Math.random()*10)%2;
+		if(chek == 1) {
+			System.out.println("employee is present");
+		}else {
+			System.out.println("employee is abscent");
+		}
+	}
+
+}
